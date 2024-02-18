10cebe4b53230154c8ff58890e8f66ed7c878dfa
leoked <65091455+leoked@users.noreply.github.com>
2024-02-18 15:11:16 -0600

    a

--- TopTitlesReducer.py
+++ TopTitlesReducer.py
@@ -8,8 +8,9 @@ for line in sys.stdin:
     if count == 10:
         break
 
-    line = line.strip().split('\t')
-    countList.append([line[0],line[1]])
+    line = line.strip()
+    countList.append(line)
+    count += 1
 
 for item in reversed(countList):
-    print('%s\t%s' % ( item[0] , item[1] ))
+    print(item)
