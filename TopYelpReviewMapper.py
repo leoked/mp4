622848ee316e4bccf4eb8d3f700b3c243540beaa
leoked <65091455+leoked@users.noreply.github.com>
2024-02-18 22:00:07 -0600

    a

--- TopYelpReviewMapper.py
+++ TopYelpReviewMapper.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
-
 count = 0
 for line in sys.stdin:
-    if count < 10:
-        line = line.strip().split('\t')
-        print('%s\t%s' % ( line[0] , line[1]))
-        count += 1
+        if count < 10:
+                line = line.strip().split('\t')
+                print('%s\t%s' % ( line[0] , line[1]))
+                count += 1
