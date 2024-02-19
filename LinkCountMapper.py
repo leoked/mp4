07784821127aa85738bf7ac50ad368df065662c4
leoked <65091455+leoked@users.noreply.github.com>
2024-02-18 18:12:27 -0600

    partd1

---
+++ LinkCountMapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+for line in sys.stdin:
+  line = line.strip().split(':')
+  fp = line[0].strip()
+  tp = line[1].strip().split(' ')
+  for p in tp:
+    if p == fp:
+      continue
+    print('%s' % ( p ))
---
+++ LinkCountReducer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+linkDict = dict()
+
+# input comes from STDIN
+for line in sys.stdin:
+    line = line.strip()
+
+    if line in linkDict:
+        linkDict[line] += 1
+    else:
+        linkDict[line] = 1
+
+sortD = dict(sorted(linkDict.items(), key = lambda item: item[1],reverse=True))
+for w in linkDict:
+    print('%s\t%s' % ( w , linkDict[w] ))
