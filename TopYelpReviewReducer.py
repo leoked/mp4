9623b0d0ff5fce0bb9740823b240fa8282df717a
leoked <65091455+leoked@users.noreply.github.com>
2024-02-18 23:09:03 -0600

    a

--- TopYelpReviewReducer.py
+++ TopYelpReviewReducer.py
@@ -4,10 +4,10 @@ import sys
 countList = dict()
 # input comes from STDIN
 for line in sys.stdin:
-     line = line.strip().split('\t')
-     countList[line[0]] = int(line[1])
+        line = line.strip().split('\t')
+        countList[line[0]] = int(line[1])
 
-sortD = dict(sorted(countList.items(), key = lambda item: item[1],reverse=True))
+sortD = dict(sorted(countList.items(), key = lambda item: item[1]))
 
 for w in sortD:
-     print('%s\t%s' % ( w , sortD[w] ))
+        print('%s\t%s' % ( w , sortD[w] ))
